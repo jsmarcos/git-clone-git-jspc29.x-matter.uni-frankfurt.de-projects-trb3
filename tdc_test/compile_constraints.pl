510f9293db4c20bd098723c2b73b82f097664808
hadaq <hadaq>
2012-06-29 09:19:27 +0000

    *** empty log message ***

--- tdc_test/compile_constraints.pl
+++ tdc_test/compile_constraints.pl
@@ -7,6 +7,6 @@ my $TOPNAME                      = "trb3_periph";  #Name of top-level entity
 my $BasePath                     = "../base/";     #path to "base" directory
 
 #create full lpf file
-system("cp $BasePath/".$TOPNAME."_ada.lpf workdir/$TOPNAME.lpf");
+system("cp $BasePath/trb3_periph_mainz.lpf workdir/$TOPNAME.lpf");
 system("cat ".$TOPNAME."_constraints.lpf >> workdir/$TOPNAME.lpf");
 
