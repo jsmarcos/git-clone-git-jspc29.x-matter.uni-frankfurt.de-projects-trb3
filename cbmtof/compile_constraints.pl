dbbd581c94efdc5d0b4fbf6ab69b46bb679fc2f0
Cahit <c.ugur@gsi.de>
2013-04-04 11:23:02 +0200

    New front end porject for the cbmtof detector designed by Jochen Fruehauf - cu

---
+++ cbmtof/compile_constraints.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use Data::Dumper;
+use warnings;
+use strict;
+
+my $TOPNAME                      = "cbmtof";  #Name of top-level entity
+
+
+#create full lpf file
+system("cp ../base/trb3_periph_cbmtof.lpf workdir/$TOPNAME.lpf");
+system("cat ../tdc_releases/tdc_v1.3/trbnet_constraints.lpf >> workdir/$TOPNAME.lpf");
+system("cat ../tdc_releases/tdc_v1.3/tdc_constraints.lpf >> workdir/$TOPNAME.lpf");
+
