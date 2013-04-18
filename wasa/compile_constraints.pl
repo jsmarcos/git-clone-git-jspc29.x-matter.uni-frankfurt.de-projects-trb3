495cb3fb91ea61b96c1673a2318ab411f0689e0a
Cahit <c.ugur@gsi.de>
2013-04-18 16:22:14 +0200

    constraints script is added - cu

--- wasa/compile_constraints.pl
+++ wasa/compile_constraints.pl
@@ -7,6 +7,6 @@ use strict;
 my $TOPNAME  = "trb3_periph_padiwa"; #Name of top-level entity
 
 #create full lpf file
-system("cp ../base/$TOPNAME.lpf workdir/$TOPNAME.lpf");
-system("cat ../tdc_releases/tdc_v1.3/trbnet_constraints.lpf >> workdir/$TOPNAME.lpf");
-system("cat ../tdc_releases/tdc_v1.3/tdc_constraints.lpf >> workdir/$TOPNAME.lpf");
+system("cp ../base/$TOPNAME.lpf diamond/$TOPNAME.lpf");
+system("cat ../tdc_releases/tdc_v1.4/trbnet_constraints.lpf >> diamond/$TOPNAME.lpf");
+system("cat ../tdc_releases/tdc_v1.4/tdc_constraints.lpf >> diamond/$TOPNAME.lpf");
