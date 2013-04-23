c80a17dad9f80f79799f020effddf1ee08933052
Andreas Neiser <neiser@kph.uni-mainz.de>
2013-04-23 09:50:58 +0200

    Switched to Synplify F-2012.03

--- cts/compile_central_gsi.pl
+++ cts/compile_central_gsi.pl
@@ -10,9 +10,9 @@ use FileHandle;
 #Settings for this project
 my $TOPNAME                      = "trb3_central";  #Name of top-level entity
 my $lattice_path                 = '/opt/lattice/diamond/2.01';
-my $synplify_path                = '/opt/synplicity/G-2012.09-SP1';
-my $lm_license_file_for_synplify = "27000\@lxcad01.gsi.de";
-my $lm_license_file_for_par      = "1702\@hadeb05.gsi.de";
+my $synplify_path                = '/opt/synplicity/F-2012.03-SP1';
+my $lm_license_file_for_synplify = '27000@lxcad01.gsi.de';
+my $lm_license_file_for_par      = '1702@hadeb05.gsi.de';
 ###################################################################################
 
 
