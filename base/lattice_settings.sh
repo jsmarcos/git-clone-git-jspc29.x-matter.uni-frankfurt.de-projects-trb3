d370df2ac0f7cd615175c7a527585b4a808e0b84
Jan Michel <j.michel@gsi.de>
2013-05-01 14:20:25 +0200

    added bindir envvar to lattice_settings

--- base/lattice_settings.sh
+++ base/lattice_settings.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # source this file to setup your shell to use lattice toolchain
-cd /opt/lattice/diamond/2.01/ispfpga && source ../bin/lin/diamond_env
+TDIR=`pwd`
+cd /opt/lattice/diamond/2.01/bin/lin
+export bindir=`pwd`
+source ./diamond_env
+cd $TDIR
+
