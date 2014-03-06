76527de1dd8e157f9b770a4bc604f256d3b65dbd
Jan Michel <j.michel@gsi.de>
2014-03-06 10:53:56 +0100

    changed to new tdc constraint file

--- 32PinAddOn/compile_constraints_frankfurt.pl
+++ 32PinAddOn/compile_constraints_frankfurt.pl
@@ -9,5 +9,5 @@ my $TOPNAME                      = "trb3_periph_32PinAddOn";  #Name of top-level
 #create full lpf file
 system("cp ../base/trb3_periph_32PinAddOn.lpf workdir/$TOPNAME.lpf");
 system("cat currentRelease/trbnet_constraints.lpf >> workdir/$TOPNAME.lpf");
-system("cat currentRelease/tdc_constraints.lpf >> workdir/$TOPNAME.lpf");
+system("cat currentRelease/tdc_constraints_2.lpf >> workdir/$TOPNAME.lpf");
 
