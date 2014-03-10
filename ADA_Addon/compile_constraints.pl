c588ea68f8d667ffde0eb032645e6067ff6b29bf
Cahit <c.ugur@gsi.de>
2014-03-10 15:51:35 +0100

    Project for ADA_Addon

---
+++ ADA_Addon/compile_constraints.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use Data::Dumper;
+use warnings;
+use strict;
+
+my $TOPNAME                      = "trb3_periph_ADA";  #Name of top-level entity
+
+
+#create full lpf file
+system("cp ../base/trb3_periph_ADA.lpf diamond/$TOPNAME.lpf");
+system("cat currentRelease/trbnet_constraints.lpf >> diamond/$TOPNAME.lpf");
+system("cat currentRelease/tdc_constraints_64.lpf >> diamond/$TOPNAME.lpf");
+
