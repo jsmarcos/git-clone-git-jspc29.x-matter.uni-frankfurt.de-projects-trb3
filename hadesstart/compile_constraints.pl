b07feed19a539133ca13f56ac23aaf8890723054
Cahit <c.ugur@gsi.de>
2014-05-06 14:52:00 +0200

    constraint files are individual for each design

--- hadesstart/compile_constraints.pl
+++ hadesstart/compile_constraints.pl
@@ -5,9 +5,9 @@ use strict;
 
 my $TOPNAME                      = "trb3_periph_32PinAddOn";  #Name of top-level entity
 
-
 #create full lpf file
-system("cp ../base/trb3_periph_32PinAddOn.lpf diamond/$TOPNAME.lpf");
-system("cat currentRelease/trbnet_constraints.lpf >> diamond/$TOPNAME.lpf");
-system("cat currentRelease/tdc_constraints.lpf >> diamond/$TOPNAME.lpf");
-
+system("cp ../base/trb3_periph_ada.lpf diamond/trb3_periph.lpf");
+system("cat currentRelease/trbnet_constraints.lpf >> diamond/trb3_periph.lpf");
+system("cat currentRelease/tdc_constraints_64.lpf >> diamond/trb3_periph.lpf");
+system("cat currentRelease/unimportant_lines_constraints.lpf >> diamond/trb3_periph.lpf");
+system("cat unimportant_lines_constraints.lpf >> diamond/trb3_periph.lpf");
