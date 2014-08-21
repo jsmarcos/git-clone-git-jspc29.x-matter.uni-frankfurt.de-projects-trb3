6f23d79bdb11b59ceaa994ace60b607084082317
Ludwig Maier <lmaier@brett.e12.ph.tum.de>
2014-08-21 15:05:57 +0200

    Revert "nxyter, new working adc handler 2"
    This reverts commit 736b5114bd1a8383608b8921db46f85e90487d37.

--- nxyter/checkerror.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "trb3_periph.twr.setup"
-grep 'Error: The following path exceeds requirements by' ./workdir/trb3_periph.twr.setup 
-echo "trb3_periph.twr.hold"
-grep 'Error:' ./workdir/trb3_periph.twr.hold
-grep 'potential circuit loops' ./workdir/*
