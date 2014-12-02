669f0c52e1a8e006842e2c2b13216f6ca89a1c8a
Andreas Neiser <neiser@kph.uni-mainz.de>
2014-12-02 09:24:07 +0100

    CTS: Compile scripts updated, syncing config.vhd

--- cts/compile_central_frankfurt_proposal.pl
+++ cts/compile_central_frankfurt_proposal.pl
@@ -16,10 +16,10 @@ my $CbmNetPath                   = "../../cbmnet";
 my $lm_license_file_for_synplify = "27000\@lxcad01.gsi.de";
 my $lm_license_file_for_par      = "1702\@hadeb05.gsi.de";
 
-my $lattice_path                 = '/opt/lattice/diamond/3.2_x64/';
-my $synplify_path                = '/opt/synplicity/I-2013.09-SP1/';
+my $lattice_path                 = '/d/jspc29/lattice/diamond/3.2_x64';
+my $synplify_path                = '/d/jspc29/lattice/synplify/I-2013.09-SP1/';
 my $lattice_bin_path             = "$lattice_path/bin/lin64"; # note the lin or lin64 at the end, no isfgpa needed
-my $config_vhd                   = 'config_mainz_a2.vhd';
+my $config_vhd                   = 'config_default.vhd';
 ###################################################################################
 
 system("./compile_constraints.pl");
