0fb2b84f35d72e65f3190cd39731f8fa152812c7
Andreas Neiser <neiser@kph.uni-mainz.de>
2015-02-18 11:53:13 +0100

    Make extra modelsim project for CFD

--- ADC/sim/tb_adcprocessor_cfd.vhd
+++ ADC/sim/tb_adcprocessor_cfd.vhd
@@ -13,16 +13,19 @@ end entity;
 
 architecture tb_arch of tb is
 
-component dummyADC is
-    generic(
-              stim_file: string :="adcsim.dat"
-            );
-    port(
-          CLK    : in  std_logic;
-          DATA   : out std_logic_vector(39 downto 0);
-          VALID  : out std_logic
-        );
-end component;
+component adc_ad9219
+  generic(NUM_DEVICES : integer := 5);
+  port(CLK            : in  std_logic;
+       CLK_ADCRAW     : in  std_logic;
+       RESTART_IN     : in  std_logic;
+       ADCCLK_OUT     : out std_logic;
+       ADC_DATA       : in  std_logic_vector(NUM_DEVICES * (CHANNELS + 1) - 1 downto 0);
+       ADC_DCO        : in  std_logic_vector(NUM_DEVICES downto 1);
+       DATA_OUT       : out std_logic_vector(NUM_DEVICES * CHANNELS * RESOLUTION - 1 downto 0);
+       FCO_OUT        : out std_logic_vector(NUM_DEVICES * RESOLUTION - 1 downto 0);
+       DATA_VALID_OUT : out std_logic_vector(NUM_DEVICES - 1 downto 0);
+       DEBUG          : out std_logic_vector(NUM_DEVICES * 32 - 1 downto 0));
+end component adc_ad9219;
 
 component adc_processor is
   generic(
@@ -54,9 +57,10 @@ component adc_processor is
     );
 end component;
 
-signal clock : std_logic := '1';
+signal clock100 : std_logic := '1';
+signal clock200 : std_logic := '1';
 signal adc_data   : std_logic_vector(39 downto 0) := (others => '0');
-signal adc_valid  : std_logic := '0';
+signal adc_valid  : std_logic_vector(0 downto 0) := (others => '1');
 signal stop_in    : std_logic := '0';
 signal trigger_out: std_logic := '0';
 signal config     : cfg_t;
@@ -64,13 +68,21 @@ signal readout_rx : READOUT_RX;
 signal readout_tx : READOUT_TX;
 signal control    : std_logic_vector(63 downto 0);
 
-signal psa_data   : std_logic_vector(8 downto 0);
-signal psa_write  : std_logic;
-signal psa_addr   : std_logic_vector(7 downto 0);
+signal psa_data   : std_logic_vector(8 downto 0) := (others => '0');
+signal psa_write  : std_logic := '0';
+signal psa_addr   : std_logic_vector(7 downto 0) := (others => '0');
+
+signal restart : std_logic := '0';
 
 begin
 
-clock <= not clock after 5 ns;
+clock100 <= not clock100 after 5 ns;
+
+clock200 <= not clock200 after 2.5 ns;
+
+restart <= '1', '0' after 200 ns;
+
+
 -- 
 -- config.buffer_depth      <= to_unsigned(100 ,11);
 -- config.samples_after     <= to_unsigned(20  ,11);
@@ -91,19 +103,21 @@ clock <= not clock after 5 ns;
 -- config.block_scale(1)    <= to_unsigned(0   , 8);      
 -- config.block_scale(2)    <= to_unsigned(0   , 8);      
 -- config.block_scale(3)    <= to_unsigned(0   , 8); 
--- config.baseline_reset_value <= to_unsigned(1023*32, 32);
+-- config.baseline_reset_value <= x"300";
 
-config.trigger_enable    <= x"0000_0000_0000", x"ffff_ffff_fff1" after 5 us;
-config.baseline_always_on <= '0'; --'1', '0' after 10 us;
+config.trigger_enable    <= x"0000_0000_0000", x"ffff_ffff_ffff" after 5 us;
+config.baseline_always_on <= '0'; --'1', '0' after 40 us;
 
-config.processing_mode   <= 1;
-config.buffer_depth      <= to_unsigned(24 ,11);
+config.processing_mode   <= 2;
+config.buffer_depth      <= to_unsigned(128 ,11);
 config.samples_after     <= to_unsigned(8  ,11);
 config.block_count       <= to_unsigned(2   , 2); 
-config.trigger_threshold <= to_unsigned(40  ,18);
-config.readout_threshold <= to_unsigned(40  ,18);
+config.trigger_threshold(15 downto 0) <= to_unsigned(30  ,16);
+config.trigger_threshold(16) <= '1'; -- invert polarity
+config.trigger_threshold(17) <= '1'; -- unused 
+config.readout_threshold <= to_unsigned(30  ,18);
 config.presum            <= to_unsigned(0   , 8);           
-config.averaging         <= to_unsigned(6   , 4);
+config.averaging         <= to_unsigned(8   , 4);
 config.block_avg(0)      <= to_unsigned(4   , 8);
 config.block_avg(1)      <= to_unsigned(1   , 8);
 config.block_avg(2)      <= to_unsigned(1   , 8);
@@ -116,11 +130,13 @@ config.block_scale(0)    <= to_unsigned(0   , 8);
 config.block_scale(1)    <= to_unsigned(0   , 8);      
 config.block_scale(2)    <= to_unsigned(0   , 8);      
 config.block_scale(3)    <= to_unsigned(0   , 8); 
-config.baseline_reset_value <= to_unsigned(2**6*1024-1, 32);
+config.baseline_reset_value <= to_unsigned(2**(to_integer(config.averaging))*3*16*16, 32);
 config.channel_disable <= (others => '0');
 config.check_word1     <= (others => '0');
 config.check_word2     <= (others => '0');
 config.check_word_enable <= '0';
+config.cfd_window <= to_unsigned(100, 8);
+config.cfd_delay <= to_unsigned(2, 4);
 
 
 readout_rx.valid_notiming_trg <= '0';
@@ -141,20 +157,20 @@ control <= (others => '0'), (8 => '1',others => '0') after 1 us, (others => '0')
 
 proc_write_psa : process begin
   wait for 1 us;
-  wait until rising_edge(clock); wait for 0.5 ns;
+  wait until rising_edge(clock100); wait for 0.5 ns;
   psa_write <= '1';
   psa_addr <= x"00";
   psa_data <= "0"&x"01";
-  wait until rising_edge(clock); wait for 0.5 ns;
+  wait until rising_edge(clock100); wait for 0.5 ns;
   psa_addr <= x"01";
   psa_data <= "0"&x"02";
-  wait until rising_edge(clock); wait for 0.5 ns;
+  wait until rising_edge(clock100); wait for 0.5 ns;
   psa_addr <= x"02";
   psa_data <= "0"&x"03";
-  wait until rising_edge(clock); wait for 0.5 ns;
+  wait until rising_edge(clock100); wait for 0.5 ns;
   psa_addr <= x"03";
   psa_data <= "1"&x"ff";
-  wait until rising_edge(clock); wait for 0.5 ns;
+  wait until rising_edge(clock100); wait for 0.5 ns;
   psa_write <= '0';
   wait;
 end process;
@@ -163,32 +179,41 @@ end process;
 proc_rdo : process begin
   readout_rx.data_valid       <= '0';
   readout_rx.valid_timing_trg <= '0';
-  wait for 15 us; wait until rising_edge(clock); wait for 0.5 ns;
+  wait for 15 us; wait until rising_edge(clock100); wait for 0.5 ns;
   readout_rx.valid_timing_trg <= '1';
-  wait until rising_edge(clock); wait for 0.5 ns;
+  wait until rising_edge(clock100); wait for 0.5 ns;
   readout_rx.valid_timing_trg <= '0';
-  wait for 250 ns; wait until rising_edge(clock); wait for 0.5 ns;
+  wait for 250 ns; wait until rising_edge(clock100); wait for 0.5 ns;
   readout_rx.data_valid       <= '1';
   wait until readout_tx.busy_release = '1';
-  wait for 10 ns; wait until rising_edge(clock); wait for 0.5 ns;
+  wait for 10 ns; wait until rising_edge(clock100); wait for 0.5 ns;
   readout_rx.data_valid       <= '0';
 end process;
 
-THE_ADC : dummyADC
-  port map(
-    CLK => clock,
-    DATA => adc_data,
-    VALID => adc_valid
-    );
+THE_ADC : adc_ad9219
+  generic map(
+    NUM_DEVICES => 1
+  )
+  port map(CLK            => clock100,
+           CLK_ADCRAW     => clock200,
+           RESTART_IN     => restart,
+           ADCCLK_OUT     => open,
+           ADC_DATA       => (others => '0'),
+           ADC_DCO        => (others => '0'),
+           DATA_OUT       => adc_data,
+           FCO_OUT        => open,
+           DATA_VALID_OUT => adc_valid,
+           DEBUG          => open
+           );
 
 UUT: adc_processor
   generic map(
     DEVICE => 0
     )
   port map(
-    CLK        => clock,
+    CLK        => clock100,
     ADC_DATA   => adc_data,
-    ADC_VALID  => adc_valid,
+    ADC_VALID  => adc_valid(0),
     STOP_IN    => stop_in,
     TRIGGER_OUT=> trigger_out,
     
@@ -211,7 +236,7 @@ UUT: adc_processor
 
     
 PROC_ADC : process begin
-  wait until rising_edge(clock); wait for 0.5 ns;
+  wait until rising_edge(clock100); wait for 0.5 ns;
 
 end process;
     
