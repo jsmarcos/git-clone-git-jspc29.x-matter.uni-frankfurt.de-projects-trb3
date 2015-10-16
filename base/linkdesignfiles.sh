9e424aaa50ed138bbaa4b29352bf521f57469cb8
Jan Michel <j.michel@gsi.de>
2015-10-16 12:07:47 +0200

    links for new design files

--- base/linkdesignfiles.sh
+++ base/linkdesignfiles.sh
@@ -21,6 +21,8 @@ ln -sf $prefix/../../../trbnet/gbe2_ecp3/ipcores_ecp3/tsmac35/pmi_ram_dpEbnoness
 ln -sf $prefix/../../../trbnet/gbe2_ecp3/ipcores_ecp3/tsmac35/pmi_ram_dpEbnonessdn96649664.ngo
 ln -sf $prefix/../../../trbnet/gbe2_ecp3/ipcores_ecp3/serdes_gbe_0ch/serdes_gbe_0ch.txt
 ln -sf $prefix/../../../trbnet/gbe2_ecp3/ipcores_ecp3/serdes_ch4.txt
+ln -sf $prefix/../../../trbnet/gbe_trb/media/serdes_gbe_4ch.txt
+
 ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/sfp_0_200_int.txt
 ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/sfp_1_200_int.txt
 ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/sfp_1_125_int.txt
@@ -28,6 +30,8 @@ ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/sfp_0_200_ctc.txt
 ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/serdes_onboard_full.txt
 ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/serdes_sync_0.txt
 ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/serdes_sync_3.txt
+ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/serdes_sync_4.txt
+ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/serdes_sync_4_slave3.txt
 ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/serdes_sync_125_0.txt
 ln -sf $prefix/../../../trbnet/media_interfaces/ecp3_sfp/serdes_full_ctc.txt
 
